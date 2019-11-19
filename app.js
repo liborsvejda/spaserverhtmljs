c1ed0a9c8dfa9f2a9c3edc4a852a59dc59b84c0e
Libor Svejda <Libor.Svejda@unicorn.com>
2019-11-19 10:31:04 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,19 @@
+const createSpaServer = require("spaserver").createSpaServer;
+
+const PORT = 8080; //aplikace na Rosti.cz musi bezet na portu 8080
+const API_HEAD = {
+    "Content-type": "application/json",
+    "Access-Control-Allow-Origin": "*"
+};
+const API_STATUS_OK = 0;
+const API_STATUS_NOT_FOUND = -1;
+
+function processApi(req, res) {
+    res.writeHead(200, API_HEAD);
+    let obj = {};
+    obj.status = API_STATUS_NOT_FOUND;
+    obj.error = "API not found";
+    res.end(JSON.stringify(obj));
+}
+
+createSpaServer(PORT, processApi);
---
+++ readme.md
@@ -0,0 +1,4 @@
+# Procvičování HTML a JavaScriptu
+
+Toto je příklad projektu na procvičování HTML a JavaScriptu s možností 
+jeho zveřejnění na hostingu http://rosti.cz. 
